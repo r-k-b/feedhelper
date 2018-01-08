36a1b38a1a150da7563166a83ca36964ac4e0f91
Robert K. Bell <robert.k.bell@gmail.com>
2018-01-08 15:27:47 +1100

    feat: add feeds that aren't already in NewsBlur

--- env.md
+++ env.md
@@ -1,3 +1,4 @@
 ```
 NEWSBLUR_SESSIONID=________________________________
+NEWSBLUR_TARGET_FOLDER=_____________
 ```
--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 const util = require('util')
 const readFile = util.promisify(fs.readFile)
+const FormData = require('form-data')
+const PQueue = require('p-queue')
+const chalk = require('chalk')
+const inspect = util.inspect
 
-const uri = 'https://newsblur.com' // 'https://newsblur-com-q9n1ynjmr1kn.runscope.net'
+const host = 'https://newsblur.com' // 'https://newsblur-com-q9n1ynjmr1kn.runscope.net'
 const sessionID = process.env['NEWSBLUR_SESSIONID']
+const targetFolder = process.env['NEWSBLUR_TARGET_FOLDER']
 
-Promise.all([getFeeds().then(showResult), getToBeImported()])
+Promise.all([getFeeds({forceFresh: true}).then(showResult), getToBeImported()])
   .then(reportImportable)
+  .then(uploadImportable)
   .catch(showError)
 
 // ---
@@ -19,7 +25,8 @@ async function getFeeds(options = {forceFresh: false}) {
   const feedsFile = './data/feeds.json'
 
   if (options.forceFresh) {
-    const response = await fetch(`${uri}/reader/feeds`, {
+    console.log(chalk.grey('Fetching fresh feed info...'))
+    const response = await fetch(`${host}/reader/feeds`, {
       headers: {
         Cookie: `newsblur_sessionid=${sessionID}`,
       },
@@ -38,12 +45,12 @@ async function getToBeImported() {
 }
 
 function showResult(result) {
-  console.log({result: summariseFeeds(result)})
+  console.log(chalk.green(inspect({result: summariseFeeds(result)})))
   return result
 }
 
 function showError(error) {
-  console.warn({error})
+  console.warn(chalk.red(inspect({error})))
 }
 
 function summariseFeeds(feeds) {
@@ -71,7 +78,51 @@ function reportImportable([feeds, toBeImported = []]) {
     uri,
     feedID: getFeedIDbyURI(feeds.feeds, uri),
   }))
-  const importable = checked.filter(checkedItem => checkedItem.feedID === null)
-  console.log({checked: checked.length, importable: importable.length})
+  const importable = checked
+    .filter(checkedItem => checkedItem.feedID === null)
+    .map(checkedItem => checkedItem.uri)
+  console.log(
+    chalk.green(
+      inspect({checked: checked.length, importable: importable.length}),
+    ),
+  )
   return {feeds, toBeImported, checked, importable}
+}
+
+async function uploadImportable({feeds, toBeImported, checked, importable}) {
+  const queue = new PQueue({concurrency: 3})
+  importable.forEach(uri => {
+    queue.add(() => addFeed(uri))
+  })
+}
+
+async function addFeed(uri) {
+  const form = new FormData()
+  form.append('url', uri)
+  form.append('folder', targetFolder)
+  console.log(chalk.grey(`Adding: ${uri}`))
+  const result = await fetch(`${host}/reader/add_url`, {
+    method: 'POST',
+    headers: {
+      Cookie: `newsblur_sessionid=${sessionID}`,
+    },
+    body: form,
+  })
+  const body = await result.json()
+  if (body.code === -1) {
+    /* Some valid Tumblr sites may return "This address does not point to an RSS feed"; they are likely
+     blocked by the 'Safe Mode' filter. Not sure if there's a way to get NewsBlur to fetch those feeds.
+     See also: https://stackoverflow.com/questions/47493273/how-to-bypass-tumblrs-safe-mode-for-rss-feeds
+    */
+    console.warn(
+      chalk.red(`Failed:  ${uri}
+    "${JSON.stringify(body)}"`),
+    )
+  } else {
+    console.log(
+      chalk.green(`Added:  ${uri}
+    "${JSON.stringify(body)}"`),
+    )
+  }
+  return result
 }
